3ede45eb868124b47bc3839803b1b32c9df5e978
grahsp <pontus.grahsp@gmail.com>
2024-11-28 22:29:37 +0100

    feat(identity): add container for identity configuration settings

---
+++ UserAuthenticationTemplate/Configs/Identity/IdentityConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace UserAuthenticationTemplate.Configs.Identity
+{
+    public class IdentityConfig
+    {
+        public PasswordConfig Password { get; set; } = new PasswordConfig();
+        public LockoutConfig Lockout { get; set; } = new LockoutConfig();
+        public SignInConfig SignIn { get; set; } = new SignInConfig();
+        public UserConfig User { get; set; } = new UserConfig();
+    }
+}
