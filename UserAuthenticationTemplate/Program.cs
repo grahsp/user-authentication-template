1fdf2ef459f294d3077fe22ad95d87fcc3bac1d3
grahsp <pontus.grahsp@gmail.com>
2024-11-28 22:19:04 +0100

    chore(config): add appsettings.Secret.json temporarily for local development

--- UserAuthenticationTemplate/Program.cs
+++ UserAuthenticationTemplate/Program.cs
@@ -10,6 +10,12 @@ namespace UserAuthenticationTemplate
             // -- Service --
             var builder = WebApplication.CreateBuilder(args);
 
+            // This should be removed before moving to production and content inside of
+            // appsettings.Secret.json Should be stored in a more secure way.
+            builder.Configuration
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.Secret.json", optional: false, reloadOnChange: true);
+
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityDbConnectionString"));
