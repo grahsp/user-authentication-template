a3110d1a6c3046066b666cf07ca96da9baa818a7
grahsp <pontus.grahsp@gmail.com>
2024-11-28 21:40:38 +0100

    feat(identity): add user and role models

---
+++ UserAuthenticationTemplate/Models/ApplicationRole.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UserAuthenticationTemplate.Models
+{
+    public class ApplicationRole : IdentityRole<Guid>
+    {
+    }
+}
---
+++ UserAuthenticationTemplate/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UserAuthenticationTemplate.Models
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+    }
+}
