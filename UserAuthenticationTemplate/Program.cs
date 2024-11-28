5b6f3e6fd68fcc827159e44e6b7d7a6112da2393
grahsp <pontus.grahsp@gmail.com>
2024-11-28 21:58:46 +0100

    feat(identity): inject database context into services

--- UserAuthenticationTemplate/Program.cs
+++ UserAuthenticationTemplate/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using UserAuthenticationTemplate.Data;
+
 namespace UserAuthenticationTemplate
 {
     public class Program
@@ -7,7 +10,12 @@ namespace UserAuthenticationTemplate
             // -- Service --
             var builder = WebApplication.CreateBuilder(args);
 
-            
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityDbConnectionString"));
+            });
+
+
             // -- Middleware --
             var app = builder.Build();
 
