604e78b203e8866d70e3a5e7421200bf1d1e1c2f
grahsp <pontus.grahsp@gmail.com>
2024-11-28 22:25:48 +0100

    feat(identity): add lockout configuration settings for user authentication

---
+++ UserAuthenticationTemplate/Configs/Identity/LockoutConfig.cs
@@ -0,0 +1,33 @@
+﻿namespace UserAuthenticationTemplate.Configs.Identity
+{
+    public class LockoutConfig
+    {
+        public bool Enabled
+        {
+            get => _lockoutTimeSpan > TimeSpan.Zero && _attempts > 0;
+        }
+
+        public bool AllowedForNewUsers { get; set; } = true;
+
+        private int _attempts = 0;
+        public int MaxFailedAccessAttempts
+        {
+            get => _attempts;
+            set => _attempts = value > 0 ? value : 0;
+        }
+
+        public int DefaultLockoutInMinutes
+        {
+            get => (int)Math.Ceiling(DefaultLockoutTimeSpan.TotalMinutes);
+            set => DefaultLockoutTimeSpan = value > 0 ? TimeSpan.FromMinutes(value) : TimeSpan.Zero;
+        }
+
+        // Timespan method should be last in order to overwrite others (incase multiple lockout times are set)
+        private TimeSpan _lockoutTimeSpan = TimeSpan.Zero;
+        public TimeSpan DefaultLockoutTimeSpan
+        {
+            get => _lockoutTimeSpan;
+            set => _lockoutTimeSpan = value > TimeSpan.Zero ? value : TimeSpan.Zero;
+        }
+    }
+}
