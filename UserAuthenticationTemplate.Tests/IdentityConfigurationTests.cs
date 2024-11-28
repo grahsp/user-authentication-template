409b53918fa2b07adc3fdff3d927329a1db28124
grahsp <pontus.grahsp@gmail.com>
2024-11-28 23:07:43 +0100

    test(identity): add unit tests to validate initialization of identity configuration objects

--- UserAuthenticationTemplate.Tests/IdentityConfigurationTests.cs
+++ UserAuthenticationTemplate.Tests/IdentityConfigurationTests.cs
@@ -1,7 +1,29 @@
+using UserAuthenticationTemplate.Configs.Identity;
+
 namespace UserAuthenticationTemplate.Tests
 {
     [TestClass]
     public class IdentityConfigurationTests
     {
+        #region General Tests
+        [TestMethod]
+        public void IdentityConfig_ShouldNotContainNullProperties()
+        {
+            var idConfig = new IdentityConfig();
+
+            Assert.IsNotNull(idConfig.Lockout);
+            Assert.IsNotNull(idConfig.Password);
+            Assert.IsNotNull(idConfig.SignIn);
+            Assert.IsNotNull(idConfig.User);
+        }
+
+        [TestMethod]
+        public void UserConfig_AllowedUsernameCharacters_ShouldNotBeEmptyOrNull()
+        {
+            var idConfig = new IdentityConfig();
+
+            Assert.IsFalse(string.IsNullOrEmpty(idConfig.User.AllowedUsernameCharacters));
+        }
+        #endregion
     }
 }
