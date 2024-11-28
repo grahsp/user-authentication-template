32e523ce2addf25d050cf00eaac1661b35c587a4
grahsp <pontus.grahsp@gmail.com>
2024-11-28 22:26:46 +0100

    feat(identity): add password configuration settings for user authentication

---
+++ UserAuthenticationTemplate/Configs/Identity/PasswordConfig.cs
@@ -0,0 +1,28 @@
+﻿namespace UserAuthenticationTemplate.Configs.Identity
+{
+    public class PasswordConfig
+    {
+        public bool RequireDigit { get; set; } = true;
+        public bool RequireLowercase { get; set; } = true;
+        public bool RequireUppercase { get; set; } = true;
+        public bool RequireNonAlphanumeric { get; set; } = false;
+
+        private int _length = 6;
+        private readonly int _lengthMin = 4;
+        public int RequiredLength
+        {
+            get => _length;
+            set => _length = value > _lengthMin ? value : _lengthMin;
+        }
+
+        private int _unique = 2;
+        private readonly int _uniqueMin = 2;
+        public int RequiredUniqueChars
+        {
+            get => _unique;
+            // _unique has to be less than _lengthMin since you can't have
+            // more unique characters than the passwords length
+            set => _unique = Math.Clamp(value, _uniqueMin, _length);
+        }
+    }
+}
