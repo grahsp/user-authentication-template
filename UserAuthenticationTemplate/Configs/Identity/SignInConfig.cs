655569529eb0331e9aefd1b218317d8ecf2272f9
grahsp <pontus.grahsp@gmail.com>
2024-11-28 22:27:38 +0100

    feat(identity): add sign-in configuration settings for user authentication

---
+++ UserAuthenticationTemplate/Configs/Identity/SignInConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace UserAuthenticationTemplate.Configs.Identity
+{
+    public class SignInConfig
+    {
+        public bool RequireConfirmedEmail { get; set; } = false;
+        public bool RequireConfirmedPhoneNumber { get; set; } = false;
+        public bool RequireConfirmedAccount { get; set; } = false;
+    }
+}
