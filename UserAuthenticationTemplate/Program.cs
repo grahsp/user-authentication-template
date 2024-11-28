4995f0227e1fd4d69bd68091ffb4f7f190845e23
grahsp <pontus.grahsp@gmail.com>
2024-11-28 22:51:58 +0100

    feat(service): inject Identity into services and configure using IdentityConfig

--- UserAuthenticationTemplate/Program.cs
+++ UserAuthenticationTemplate/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using UserAuthenticationTemplate.Configs.Identity;
+using UserAuthenticationTemplate.Data;
+using UserAuthenticationTemplate.Models;
 
 namespace UserAuthenticationTemplate
 {
@@ -19,6 +23,48 @@ namespace UserAuthenticationTemplate
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.Secret.json", optional: false, reloadOnChange: true);
 
+
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityDbConnectionString"));
+            });
+
+            builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+            {
+                var identityConfig = identityConfigurationSection.Get<IdentityConfig>() ?? new IdentityConfig();
+
+                // Password
+                var password = identityConfig.Password;
+
+                options.Password.RequireDigit = password.RequireDigit;
+                options.Password.RequireLowercase = password.RequireLowercase;
+                options.Password.RequireUppercase = password.RequireUppercase;
+                options.Password.RequireNonAlphanumeric = password.RequireNonAlphanumeric;
+                options.Password.RequiredLength = password.RequiredLength;
+                options.Password.RequiredUniqueChars = password.RequiredUniqueChars;
+
+                // Lockout
+                var lockout = identityConfig.Lockout;
+
+                options.Lockout.AllowedForNewUsers = lockout.AllowedForNewUsers;
+                options.Lockout.MaxFailedAccessAttempts = lockout.MaxFailedAccessAttempts;
+                options.Lockout.DefaultLockoutTimeSpan = lockout.DefaultLockoutTimeSpan;
+
+                // Sign in
+                var signIn = identityConfig.SignIn;
+
+                options.SignIn.RequireConfirmedEmail = signIn.RequireConfirmedEmail;
+                options.SignIn.RequireConfirmedPhoneNumber = signIn.RequireConfirmedPhoneNumber;
+                options.SignIn.RequireConfirmedAccount = signIn.RequireConfirmedPhoneNumber;
+
+                // User
+                var user = identityConfig.User;
+
+                options.User.AllowedUserNameCharacters = user.AllowedUsernameCharacters;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
 
 
             // -- Middleware --
