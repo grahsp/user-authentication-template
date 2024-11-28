849ab5e726333a116fd3700e9888f411ee3c569a
grahsp <pontus.grahsp@gmail.com>
2024-11-28 21:41:41 +0100

    feat(identity): add database context with basic config for user and role

---
+++ UserAuthenticationTemplate/Data/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UserAuthenticationTemplate.Models;
+
+namespace UserAuthenticationTemplate.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // User
+            builder.Entity<ApplicationUser>()
+                .HasIndex(u => u.NormalizedUserName)
+                .IsUnique();
+
+            builder.Entity<ApplicationUser>()
+                .Property(u => u.Email)
+                .IsRequired();
+
+            builder.Entity<ApplicationUser>()
+                .HasIndex(u => u.NormalizedEmail)
+                .IsUnique();
+
+
+            // Role
+            builder.Entity<ApplicationRole>().HasData(
+                new ApplicationRole { Id = Guid.NewGuid(), Name = "Admin", NormalizedName = "ADMIN" },
+                new ApplicationRole { Id = Guid.NewGuid(), Name = "Moderator", NormalizedName = "MODERATOR" },
+                new ApplicationRole { Id = Guid.NewGuid(), Name = "User", NormalizedName = "USER" },
+                new ApplicationRole { Id = Guid.NewGuid(), Name = "Guest", NormalizedName = "GUEST" }
+            );
+
+            builder.Entity<ApplicationRole>()
+                .HasIndex(r => r.NormalizedName)
+                .IsUnique();
+        }
+    }
+}
