9796b41b180e09d079a16963f127d5f946e8e27d
grahsp <pontus.grahsp@gmail.com>
2024-12-01 15:18:50 +0100

    feat(user-registration): create a dto with data annotations for a registration request

---
+++ UserAuthenticationTemplate/Models/RegistrationRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserAuthenticationTemplate.Models
+{
+    public class RegistrationRequest
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public required string Password { get; set; }
+
+        [Required(ErrorMessage = "Password confirmation is required.")]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public required string ConfirmPassword { get; set; }
+    }
+}
