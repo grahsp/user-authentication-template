254c32bff76f099a47b018a9f3a0cd08c9964f20
grahsp <pontus.grahsp@gmail.com>
2024-12-01 13:42:00 +0100

    feat(user-management): create class implementing interface IUserManager

---
+++ UserAuthenticationTemplate/Services/UserAccountManager.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using UserAuthenticationTemplate.Models;
+
+namespace UserAuthenticationTemplate.Services
+{
+    public class UserAccountManager : IUserManager<ApplicationUser>
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserAccountManager(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public Task<bool> CheckPasswordAsync(ApplicationUser user, string password)
+        {
+            return _userManager.CheckPasswordAsync(user, password);
+        }
+
+        public Task<IdentityResult> CreateAsync(ApplicationUser user, string password)
+        {
+            return _userManager.CreateAsync(user, password);
+        }
+
+        public Task<ApplicationUser?> FindByEmailAsync(string email)
+        {
+            return _userManager.FindByEmailAsync(email);
+        }
+
+        public Task<ApplicationUser?> FindByIdAsync(string id)
+        {
+            return _userManager.FindByIdAsync(id);
+        }
+
+        public Task<ApplicationUser?> FindByNameAsync(string username)
+        {
+            return _userManager.FindByNameAsync(username);
+        }
+    }
+}
