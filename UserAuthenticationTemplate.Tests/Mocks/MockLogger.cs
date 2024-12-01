babe8ab0488a72dc8aed392b174342b2b97133a9
grahsp <pontus.grahsp@gmail.com>
2024-12-01 18:37:06 +0100

    test(auth): create mocks to test UserAccountService

---
+++ UserAuthenticationTemplate.Tests/Mocks/MockLogger.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace UserAuthenticationTemplate.Tests.Mocks
+{
+    internal class MockLogger<T> : ILogger<T>
+    {
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            if (formatter != null)
+            {
+                string logMessage = formatter(state, exception);
+                Debug.WriteLine($"[{logLevel}] {logMessage}");
+            }
+        }
+    }
+}
---
+++ UserAuthenticationTemplate.Tests/Mocks/MockUserManager.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using UserAuthenticationTemplate.Models;
+using UserAuthenticationTemplate.Services;
+
+namespace UserAuthenticationTemplate.Tests.Mocks
+{
+    internal class MockUserManager : IUserManager<ApplicationUser>
+    {
+        private List<ApplicationUser> _users = [];
+
+        public Task<bool> CheckPasswordAsync(ApplicationUser user, string password)
+        {
+            return Task.FromResult(user.PasswordHash == password);
+        }
+
+        public Task<IdentityResult> CreateAsync(ApplicationUser user, string password)
+        {
+            var userExists = _users.Exists(u => u.Email == user.Email);
+            if (userExists)
+                return Task.FromResult(IdentityResult.Failed(new IdentityError { Description = "User already exists!" }));
+
+            user.PasswordHash = password;
+            _users.Add(user);
+
+            return Task.FromResult(IdentityResult.Success);
+        }
+
+        public Task<ApplicationUser?> FindByEmailAsync(string email)
+        {
+            var user = _users.FirstOrDefault(u => u.Email == email);
+            return Task.FromResult(user);
+        }
+
+        public Task<ApplicationUser?> FindByIdAsync(string id)
+        {
+            var user = _users.FirstOrDefault(u => u.Id.ToString() == id);
+            return Task.FromResult(user);
+        }
+
+        public Task<ApplicationUser?> FindByNameAsync(string username)
+        {
+            var user = _users.FirstOrDefault(u => u.UserName == username);
+            return Task.FromResult(user);
+        }
+    }
+}
