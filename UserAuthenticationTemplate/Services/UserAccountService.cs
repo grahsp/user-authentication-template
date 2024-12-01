ea8518e01e958456ab2ec9c77ea5939303a1b709
grahsp <pontus.grahsp@gmail.com>
2024-12-01 15:12:59 +0100

    feat(user-management): create UserAccountService to handle high-level user management operations

---
+++ UserAuthenticationTemplate/Services/UserAccountService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Options;
+using UserAuthenticationTemplate.Configs.Identity;
+using UserAuthenticationTemplate.Models;
+
+namespace UserAuthenticationTemplate.Services
+{
+    public class UserAccountService
+    {
+        private readonly IUserManager<ApplicationUser> _userManager;
+        private readonly ILogger<UserAccountService> _logger;
+        private readonly IdentityConfig _identityConfig;
+
+        public UserAccountService(IUserManager<ApplicationUser> userManager, ILogger<UserAccountService> logger, IOptions<IdentityConfig> identityConfig)
+        {
+            _userManager = userManager;
+            _logger = logger;
+            _identityConfig = identityConfig.Value;
+        }
+    }
+}
