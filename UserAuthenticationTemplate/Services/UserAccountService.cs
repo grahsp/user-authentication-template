cfff0f4dbf9ec6301504fc34b2387e44824012fa
grahsp <pontus.grahsp@gmail.com>
2024-12-03 10:42:10 +0100

    feat(auth): refactor UserAccountService with seperate methods to follow SRP, improve error handling and logging
     - Create separate methods with try-catching to avoid unhandled exceptions propagating from database-related operations.
     - Improve logging and replace mentions of user email and replace it with the user id for privacy.

--- UserAuthenticationTemplate/Services/UserAccountService.cs
+++ UserAuthenticationTemplate/Services/UserAccountService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
+using System.Text.RegularExpressions;
 using UserAuthenticationTemplate.Configs.Identity;
 using UserAuthenticationTemplate.Models;
 
@@ -11,6 +12,8 @@ namespace UserAuthenticationTemplate.Services
         private readonly ILogger<UserAccountService> _logger;
         private readonly IdentityConfig _identityConfig;
 
+        private bool LockoutEnabled => _identityConfig.Lockout.Enabled;
+
         public UserAccountService(IUserManager<ApplicationUser> userManager, ILogger<UserAccountService> logger, IOptions<IdentityConfig> identityConfig)
         {
             _userManager = userManager;
@@ -19,10 +22,10 @@ namespace UserAuthenticationTemplate.Services
         }
 
         public async Task<IdentityResult> RegisterUserAsync(RegistrationRequest request)
-        {
-            if (request.Password != request.ConfirmPassword)
             {
-                _logger.LogWarning("Failed to register '{Email}' due to a password missmatch", request.Email);
+            if (!string.Equals(request.Password, request.ConfirmPassword))
+            {
+                _logger.LogWarning("Passwords did not match during registration attempt.");
                 return IdentityResult.Failed(new IdentityError { Description = "Passwords do not match." });
             }
 
@@ -31,43 +34,174 @@ namespace UserAuthenticationTemplate.Services
                 Email = request.Email
             };
 
-            var result = await _userManager.CreateAsync(newUser, request.Password);
-
-            if (result.Succeeded)
-            {
-                // Logic that should execute if successful goes here..
-                _logger.LogInformation("User '{Email}' successfully registered.", newUser.Email);
-            }
-            else
-            {
-                _logger.LogWarning("User '{Email}' failed to register!", newUser.Email);
-            }
+            // Handle success vs failure here..
+            var result = await CreateAsync(newUser, request.Password);
 
             return result;
         }
 
+        private async Task<IdentityResult> CreateAsync(ApplicationUser user, string password)
+        {
+            try
+            {
+                var result = await _userManager.CreateAsync(user, password);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User with ID '{UserId}' successfully created.", user.Id);
+                }
+                else
+                {
+                    _logger.LogWarning("Failed to create user with ID '{UserId}'", user.Id);
+
+                    foreach (var error in result.Errors)
+                    {
+                        _logger.LogWarning(" - {ErrorDescription}", error.Description);
+                    }
+                }
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while trying to create user with ID '{UserId}'.", user.Id);
+                return IdentityResult.Failed(new IdentityError { Description = "An error occurred while creating your account. Please try again later." });
+            }
+        }
+
         public async Task<IdentityResult> LoginUserAsync(LoginRequest request)
         {
-            var user = await _userManager.FindByEmailAsync(request.Email);
+            var user = await FindUserAsync(request.Email);
             if (user == null)
             {
-                _logger.LogWarning("User '{Email}' not found.", request.Email);
-                return IdentityResult.Failed(new IdentityError { Description = $"User '{ request.Email }' not found." });
+                return IdentityResult.Failed(new IdentityError { Description = "Invalid login attempt." });
             }
 
-            var isPasswordValid = await _userManager.CheckPasswordAsync(user, request.Password);
-
-            if (isPasswordValid)
+            if (await IsUserLockedOutAsync(user))
             {
-                // Logic that should execute if successful goes here..
-                _logger.LogInformation("User '{Email}' succesfully logged in.", user.Email);
+                return IdentityResult.Failed(new IdentityError { Description = "Your account is temporarily locked due to multiple failed login attempts. Please try again later." });
+            }
 
+            if (await CheckPasswordAsync(user, request.Password))
+            {
+                // Success
                 return IdentityResult.Success;
             }
             else
             {
-                _logger.LogWarning("User '{Email}' failed to login with incorrect password.", user.Email);
-                return IdentityResult.Failed(new IdentityError { Description = $"User '{user.Email} failed to login." });
+                _ = await AccessFailedCountAsync(user);
+                return IdentityResult.Failed(new IdentityError { Description = "Invalid login attempt. Please check your username and password and try again." });
+            }
+        }
+
+        private async Task<ApplicationUser?> FindUserAsync(string identifier)
+        {
+            if (string.IsNullOrEmpty(identifier))
+            {
+                _logger.LogError("Parameter identifier cannot be empty or null.");
+                return null;
+            }
+
+            try
+            {
+                ApplicationUser? user;
+                if (IsValidEmail(identifier))
+                {
+                    user = await _userManager.FindByEmailAsync(identifier);
+                    if (user == null)
+                        _logger.LogWarning("No user found with Email: '{Identifier}'", identifier);
+                }
+                else
+                {
+                    user = await _userManager.FindByNameAsync(identifier);
+                    if (user == null)
+                        _logger.LogWarning("No user found with Username: '{Identifier}'", identifier);
+                }
+
+                if (user != null)
+                    _logger.LogInformation("User found with ID '{UserId}'", user.Id);
+
+                return user;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while trying to find user with identifier '{Identifier}'.", identifier);
+                return null;
+            }
+        }
+
+        private static bool IsValidEmail(string identifier)
+        {
+            var emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(identifier, emailPattern);
+        }
+
+        private async Task<bool> CheckPasswordAsync(ApplicationUser user, string password)
+        {
+            try
+            {
+                var result = await _userManager.CheckPasswordAsync(user, password);
+                if (result)
+                {
+                    _logger.LogInformation("Successfully authenticated user with ID '{UserId}'.", user.Id);
+                }
+                else
+                {
+                    _logger.LogWarning("Incorrect password provided for user with ID '{UserId}'.", user.Id);
+                }
+
+                return result;
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while comparing passwords for user with ID '{UserId}'.", user.Id);
+                return false;
+            }
+        }
+
+        private async Task<bool> IsUserLockedOutAsync(ApplicationUser user)
+        {
+            try
+            {
+                var isLockedOut = await _userManager.IsLockedOutAsync(user) && LockoutEnabled;
+                if (isLockedOut)
+                {
+                    _logger.LogInformation("User with ID '{UserId}' is locked out.", user.Id);
+                }
+                else
+                {
+                    _logger.LogInformation("User with ID '{UserId}' is not locked out.", user.Id);
+                }
+
+                return isLockedOut;
+            }
+            catch (Exception ex)
+            {
+                // This will allow blocked users in if an error occurs (change result type or re-throw)
+                _logger.LogError(ex, "An error occurred while checking lockout state for user with ID '{UserId}'.", user.Id);
+                return false;
+            }
+        }
+
+        private async Task<bool> AccessFailedCountAsync(ApplicationUser user)
+        {
+            try
+            {
+                var result = await _userManager.AccessFailedCountAsync(user);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("Successfully retrieved failed login count for user with ID '{UserId}'.", user.Id);
+                }
+                else
+                {
+                    _logger.LogWarning("Failed to retrieve failed login count for user with ID '{UserId}'.", user.Id);
+                }
+
+                return result.Succeeded && LockoutEnabled;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while accessing the failed login count for user with ID '{UserId}'.", user.Id);
+                return false;
             }
         }
     }
