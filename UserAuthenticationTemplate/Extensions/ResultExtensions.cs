2c293d381e2e291e8b2508e39a8f020d6dfd7e57
grahsp <pontus.grahsp@gmail.com>
2024-12-10 09:06:28 +0100

    reafactor(result): move ResultExtensions to separate file

---
+++ UserAuthenticationTemplate/Extensions/ResultExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UserAuthenticationTemplate.Models;
+
+namespace UserAuthenticationTemplate.Extensions
+{
+    public static class ResultExtensions
+    {
+        public static Result<T> CreateResult<T>(this T? data, string failureMessage = "Data must not be null")
+        {
+            return data != null
+                ? Result<T>.Success(data)
+                : Result<T>.Failure(failureMessage);
+        }
+    }
+}
--- UserAuthenticationTemplate/Models/Result.cs
+++ UserAuthenticationTemplate/Models/Result.cs
@@ -84,14 +84,4 @@
                 : $"Failure: {string.Join(", ", Errors)}";
         }
     }
-
-    public static class ResultExtensions
-    {
-        public static Result<T> CreateResult<T>(this T? data, string failureMessage = "Data must not be null")
-        {
-            return data != null
-                ? Result<T>.Success(data)
-                : Result<T>.Failure(failureMessage);
-        }
-    }
 }
