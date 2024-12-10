2558ed307de85f7e78a72e98662134f5cd8826b5
grahsp <pontus.grahsp@gmail.com>
2024-12-10 09:16:53 +0100

    test(result): add unit tests for ResultExtensions

---
+++ UserAuthenticationTemplate.Tests/ExtensionsTests.cs
@@ -0,0 +1,60 @@
+﻿using UserAuthenticationTemplate.Extensions;
+
+namespace UserAuthenticationTemplate.Tests
+{
+    [TestClass]
+    public class ExtensionsTests
+    {
+        #region ResultExtensions
+        [TestMethod]
+        public void CreateResult_ReturnsSuccess_WhenDataIsNotNull()
+        {
+            var data = 42;
+
+            var result = data.CreateResult();
+
+            Assert.IsTrue(result.IsSuccess);
+            Assert.AreEqual(data, result.Data);
+        }
+
+        [TestMethod]
+        public void CreateResult_ReturnsFailure_WhenDataIsNull()
+        {
+            int? data = null;
+
+            var result = data.CreateResult();
+
+            Assert.IsTrue(result.IsFailure);
+            Assert.AreEqual("Data must not be null", result.Errors.Single());
+        }
+
+        [TestMethod]
+        public void CreateResult_UsesCustomFailureMessage_WhenDataIsNull()
+        {
+            string? data = null;
+            string errorMessage = "An error occured";
+
+            var result = data.CreateResult(errorMessage);
+
+            Assert.IsTrue(result.IsFailure);
+            Assert.AreEqual(errorMessage, result.Errors.Single());
+        }
+
+        [TestMethod]
+        public void CreateResult_ReturnsSuccess_WithDifferentTypes()
+        {
+            var intData = 42;
+            var stringData = "Hello, World!";
+
+            var intResult = intData.CreateResult();
+            var stringResult = stringData.CreateResult();
+
+            Assert.IsTrue(intResult.IsSuccess);
+            Assert.AreEqual(intData, intResult.Data);
+
+            Assert.IsTrue(stringResult.IsSuccess);
+            Assert.AreEqual(stringData, stringResult.Data);
+        }
+        #endregion
+    }
+}
