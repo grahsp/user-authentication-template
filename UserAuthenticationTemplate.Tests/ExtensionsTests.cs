2a91c9e29b0697963be247828d1bd1546663a53f
grahsp <pontus.grahsp@gmail.com>
2024-12-10 09:30:11 +0100

    test(identity): add unit tests for IdentityResultExtensions for converting into Result

--- UserAuthenticationTemplate.Tests/ExtensionsTests.cs
+++ UserAuthenticationTemplate.Tests/ExtensionsTests.cs
@@ -1,10 +1,13 @@
-﻿using UserAuthenticationTemplate.Extensions;
+﻿using Microsoft.AspNetCore.Identity;
+using UserAuthenticationTemplate.Extensions;
+using UserAuthenticationTemplate.Models;
 
 namespace UserAuthenticationTemplate.Tests
 {
     [TestClass]
     public class ExtensionsTests
     {
+
         #region ResultExtensions
         [TestMethod]
         public void CreateResult_ReturnsSuccess_WhenDataIsNotNull()
@@ -54,6 +57,71 @@ namespace UserAuthenticationTemplate.Tests
 
             Assert.IsTrue(stringResult.IsSuccess);
             Assert.AreEqual(stringData, stringResult.Data);
+        }
+        #endregion
+
+        #region IdentityResultsExtensions
+        // Result
+        [TestMethod]
+        public void ToResult_ReturnsSuccess_WhenIdentityResultIsSuccessful()
+        {
+            var identityResult = IdentityResult.Success;
+
+            var result = identityResult.ToResult();
+
+            Assert.IsInstanceOfType<Result>(result);
+            Assert.IsTrue(result.IsSuccess);
+            Assert.IsFalse(result.HasErrors);
+        }
+
+        [TestMethod]
+        public void ToResult_ReturnsFailure_WhenIdentityResultFails()
+        {
+            var errors = new[] { new IdentityError { Description = "Error 1" }, new IdentityError { Description = "Error 2" } };
+            var identityResult = IdentityResult.Failed(errors);
+
+            var result = identityResult.ToResult();
+
+            Assert.IsTrue(result.IsFailure);
+            CollectionAssert.AreEquivalent(new[] { "Error 1", "Error 2" }, result.Errors);
+        }
+
+        [TestMethod]
+        public void ToResult_HandlesEmptyErrors_WhenIdentityResultFails()
+        {
+            var identityResult = IdentityResult.Failed();
+
+            var result = identityResult.ToResult();
+
+            Assert.IsTrue(result.IsFailure);
+            Assert.IsFalse(result.HasErrors);
+        }
+
+        // Result<T>
+        [TestMethod]
+        public void ToResultT_ReturnsSuccess_WhenIdentityResultIsSuccessful()
+        {
+            var identityResult = IdentityResult.Success;
+            var data = 42;
+
+            var result = identityResult.ToResult(data);
+
+            Assert.IsTrue(result.IsSuccess);
+            Assert.AreEqual(data, result.Data);
+        }
+
+        [TestMethod]
+        public void ToResultT_ReturnsFailure_WhenIdentityResultFails()
+        {
+            var errors = new[] { new IdentityError { Description = "Error 1" }, new IdentityError { Description = "Error 2" } };
+            var identityResult = IdentityResult.Failed(errors);
+            var data = 42;
+
+            var result = identityResult.ToResult(data);
+
+            Assert.IsTrue(result.IsFailure);
+            Assert.ThrowsException<ArgumentNullException>(() => result.Data);
+            CollectionAssert.AreEquivalent(new[] { "Error 1", "Error 2" }, result.Errors);
         }
         #endregion
     }
