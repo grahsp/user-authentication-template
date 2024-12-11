b2462274b0e82dad5f24c3e9eac69b77d8e9d7d4
grahsp <pontus.grahsp@gmail.com>
2024-12-11 12:28:55 +0100

    feat(logging): add log method for when arguments are null

---
+++ UserAuthenticationTemplate/Logging/LogCommon.cs
@@ -0,0 +1,13 @@
+﻿namespace UserAuthenticationTemplate.Logging
+{
+    public static partial class Log
+    {
+        [LoggerMessage(EventId = 0, Level = LogLevel.Warning, Message = "The following required arguments are null: '{Arguments}'.")]
+        private static partial void ArgumentNull(ILogger logger, string arguments);
+
+        public static void LogArgumentNull(this ILogger logger, params string[] arguments)
+        {
+            ArgumentNull(logger, string.Join(", ", arguments.Select(a => a)));
+        }
+    }
+}
