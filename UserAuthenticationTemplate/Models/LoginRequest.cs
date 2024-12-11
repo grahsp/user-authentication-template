568bc8d3521788c822415c5c3c77f8203d0da763
grahsp <pontus.grahsp@gmail.com>
2024-12-11 12:28:12 +0100

    update(dto): add both email and username as optional to allow either to be passed
     - Removed confirm password property since user might not require it. Let the user extend DTO to add it along with validatio  logic.

--- UserAuthenticationTemplate/Models/LoginRequest.cs
+++ UserAuthenticationTemplate/Models/LoginRequest.cs
@@ -4,9 +4,10 @@ namespace UserAuthenticationTemplate.Models
 {
     public class LoginRequest
     {
-        [Required(ErrorMessage = "Email is required.")]
         [EmailAddress(ErrorMessage = "Invalid email address.")]
-        public required string Email { get; set; }
+        public string? Email { get; set; }
+
+        public string? UserName { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
         public required string Password { get; set; }
--- UserAuthenticationTemplate/Models/RegistrationRequest.cs
+++ UserAuthenticationTemplate/Models/RegistrationRequest.cs
@@ -4,15 +4,12 @@ namespace UserAuthenticationTemplate.Models
 {
     public class RegistrationRequest
     {
-        [Required(ErrorMessage = "Email is required.")]
         [EmailAddress(ErrorMessage = "Invalid email address.")]
-        public required string Email { get; set; }
+        public string? Email { get; set; }
+
+        public string? UserName { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
         public required string Password { get; set; }
-
-        [Required(ErrorMessage = "Password confirmation is required.")]
-        [Compare("Password", ErrorMessage = "Passwords do not match.")]
-        public required string ConfirmPassword { get; set; }
     }
 }
