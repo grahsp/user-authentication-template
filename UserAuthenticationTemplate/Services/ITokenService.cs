f0accecc66b6c90c42399d34745609085b94bb53
grahsp <pontus.grahsp@gmail.com>
2024-12-14 14:35:07 +0100

    feat(token-service): create facade for token service classes

---
+++ UserAuthenticationTemplate/Services/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+using UserAuthenticationTemplate.Models;
+
+namespace UserAuthenticationTemplate.Services
+{
+    public interface ITokenService
+    {
+        TokenResult GenerateToken(IEnumerable<Claim> claims);
+        TokenValidationResult ValidateToken(string token);
+    }
+}
