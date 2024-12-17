f6dded2ed99321e758226d8361e8950aad55f4da
grahsp <pontus.grahsp@gmail.com>
2024-12-17 20:12:19 +0100

    refactor(services): implement ServiceConfiguration into Program

--- UserAuthenticationTemplate/Program.cs
+++ UserAuthenticationTemplate/Program.cs
@@ -1,13 +1,9 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using UserAuthenticationTemplate.Configs;
-using UserAuthenticationTemplate.Configs.Identity;
 using UserAuthenticationTemplate.Data;
 using UserAuthenticationTemplate.Models;
-using UserAuthenticationTemplate.Services;
 
 namespace UserAuthenticationTemplate
 {
@@ -18,14 +14,9 @@ namespace UserAuthenticationTemplate
             // -- Service Configuration -- //
             var builder = WebApplication.CreateBuilder(args);
 
-            #region Configuration
             builder.Configuration.AddUserSecrets<Program>();
 
-            var identityConfigurationSection = builder.Configuration.GetSection("Identity");
-            builder.Services.Configure<IdentityConfig>(identityConfigurationSection);
-            var jwtConfigurationSection = builder.Configuration.GetSection("Jwt");
-            builder.Services.Configure<JwtConfig>(jwtConfigurationSection);
-            #endregion
+            builder.Services.AddControllers();
 
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
             {
@@ -33,53 +24,12 @@ namespace UserAuthenticationTemplate
                 options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityDbConnectionString"));
             });
 
-            #region Register Services
-            builder.Services.AddScoped<ITokenService, JwtService>();
-            builder.Services.AddScoped<IUserManager<ApplicationUser>, UserAccountManager>();
-            builder.Services.AddScoped<UserAccountService>();
-
-            builder.Services.AddControllers();
-            #endregion
-
-            #region Identity Setup
             builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
-            {
-                var identityConfig = identityConfigurationSection.Get<IdentityConfig>() ?? new IdentityConfig();
-
-                // Password
-                var password = identityConfig.Password;
-
-                options.Password.RequireDigit = password.RequireDigit;
-                options.Password.RequireLowercase = password.RequireLowercase;
-                options.Password.RequireUppercase = password.RequireUppercase;
-                options.Password.RequireNonAlphanumeric = password.RequireNonAlphanumeric;
-                options.Password.RequiredLength = password.RequiredLength;
-                options.Password.RequiredUniqueChars = password.RequiredUniqueChars;
-
-                // Lockout
-                var lockout = identityConfig.Lockout;
-
-                options.Lockout.AllowedForNewUsers = lockout.AllowedForNewUsers;
-                options.Lockout.MaxFailedAccessAttempts = lockout.MaxFailedAccessAttempts;
-                options.Lockout.DefaultLockoutTimeSpan = lockout.DefaultLockoutTimeSpan;
-
-                // Sign in
-                var signIn = identityConfig.SignIn;
-
-                options.SignIn.RequireConfirmedEmail = signIn.RequireConfirmedEmail;
-                options.SignIn.RequireConfirmedPhoneNumber = signIn.RequireConfirmedPhoneNumber;
-                options.SignIn.RequireConfirmedAccount = signIn.RequireConfirmedPhoneNumber;
-
-                // User
-                var user = identityConfig.User;
-
-                options.User.AllowedUserNameCharacters = user.AllowedUsernameCharacters;
-            })
+                options.FromConfig(builder.Configuration)
+            )
             .AddEntityFrameworkStores<ApplicationDbContext>()
             .AddDefaultTokenProviders();
-            #endregion
 
-            #region Authentication Setup
             builder.Services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -87,26 +37,10 @@ namespace UserAuthenticationTemplate
             })
             .AddJwtBearer(options =>
             {
-                var jwtConfig = jwtConfigurationSection.Get<JwtConfig>();
-
-                if (string.IsNullOrEmpty(jwtConfig?.Secret))
-                    throw new InvalidOperationException("JWT Secret is missing from configuration!");
-
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = jwtConfig.ValidateIssuer,
-                    ValidateAudience = jwtConfig.ValidateAudience,
-                    ValidIssuer = jwtConfig.Issuer,
-                    ValidAudience = jwtConfig.Audience,
-                    ClockSkew = jwtConfig.ClockSkew,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfig.Secret))
-                };
+                options.FromConfig(builder.Configuration);
             });
 
             builder.Services.AddAuthorization();
-            #endregion
 
 
             // -- Middleware Configuration -- //
