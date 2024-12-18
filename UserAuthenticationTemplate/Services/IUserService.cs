3d791896fba8a90dcb880dd9f360409c37ea9b07
grahsp <pontus.grahsp@gmail.com>
2024-12-18 10:00:11 +0100

    feat(auth): create interface to be used by authentication services

---
+++ UserAuthenticationTemplate/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using UserAuthenticationTemplate.Models;
+
+namespace UserAuthenticationTemplate.Services
+{
+    public interface IUserService
+    {
+        Task<Result<RegisterResponse>> RegisterUserAsync(RegistrationRequest request);
+        Task<Result<LoginResponse>> LoginUserAsync(LoginRequest request);
+    }
+}
